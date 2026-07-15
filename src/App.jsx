789eed54e9bbd59e70610fcc0e00efc523407402
Trey Graham <trey@Treys-MacBook-Pro.local>
2026-07-15 16:11:41 -0500

    Hotfix: Pages Not Opening at Top

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Home from './pages/Home.jsx'
 import AboutMe from './pages/AboutMe.jsx'
 import Lessons from './pages/Lessons.jsx'
 import Testimonials from './pages/Testimonials.jsx'
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutMe />} />
-      <Route path="/lessons" element={<Lessons />} />
-      <Route path="/testimonials" element={<Testimonials />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+      </Routes>
+    </>
   )
 }
 
