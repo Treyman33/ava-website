f2cc9a01a11d3f9deddd7ff5011f8861c44e5b0b
Trey Graham <trey@Treys-MacBook-Pro.local>
2026-07-24 08:15:17 -0500

    Hotfix: About Me Subpage text edit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -28,7 +28,7 @@ function AboutMe() {
         <div className = "am-content-1">
             <h2>My Story:</h2>
             <p>Hi, my name is Ava Marcum! I am 22 years old and have been teaching swimming lessons every summer for the past 7 years.
-                I originally began to teach swimming lessons when I worked at Aqua Tots Swim School. After working there for *INSERT TIME HERE*,
+                I originally began to teach swimming lessons when I worked at Aqua Tots Swim School. After working there for 1 year,
                  I decided to begin giving private lessons as I felt that more indidivualized lessons would be more beneficial for the kids.
                 I have helped over 100 kids learn to swim, and am excited to teach your kids to swim as well!
             </p>
